5f628f2436b48553180a30bc376ae749ddcedf34
EBlTEN <228321068+icvt408@users.noreply.github.com>
2025-08-24 20:17:41 +0900

    delete utils

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,11 @@ import Header from './components/Header'
 import PlayerCard from "./components/PlayerCard"
 import RankModal from './components/RankModal'
 import Toast from './components/Toast'
-import { getAccountByRiotId, getLeagueByPuuid } from './utils/riotApi'
 
 function App() {
 
   const [players, setPlayers] = useState([])
   const [modalPlayerName, setModalPlayerName] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
   const [toastMessage, setToastMessage] = useState(null)
 
   const showToast = (message) => {
@@ -70,51 +68,6 @@ function App() {
     handleCloseRankModal()
   }
 
-
-  // APIから不足情報を取得するボタン
-  const handleComplatePlayerInfo = async () => {
-    setIsLoading(true);
-
-    const playersToUpdate = players.filter(player => player.rank.tier === "Unranked");
-
-    if (playersToUpdate.length === 0) {
-      setIsLoading(false);
-      return;
-    }
-
-    const updatePromiss = playersToUpdate.map(async (player) => {
-      try {
-        const accoundData = await getAccountByRiotId(player.game_name, player.tagline);
-        const puuid = accoundData.puuid;
-
-        const leagueData = await getLeagueByPuuid(puuid);
-        console.log(leagueData)
-        const rankData = Array.from(leagueData).find(rankData => rankData.queueType === "RANKED_SOLO_5x5");
-
-
-        return {
-          ...player,
-          rank: rankData === undefined ? { rank: rankData.rank, tier: rankData.division } : { tier: "Unranked", division: "I" }
-        }
-
-      } catch (error) {
-        showToast("APIリクエスト中にエラーが発生しました。")
-        console.error(`Failed to fetch data for ${player.name}:`, error);
-        setIsLoading(false);
-      }
-    });
-
-    const updatedPlayerData = await Promise.all(updatePromiss);
-
-    const newPlayers = players.map(player => {
-      const updatedPlayer = updatedPlayerData.find(up => up.name === player.name);
-      return updatedPlayer || player;
-    });
-
-    setPlayers(newPlayers);
-    setIsLoading(false);
-  }
-
   const playerInModal = players.find(p => p.name === modalPlayerName);
 
 
@@ -137,8 +90,6 @@ function App() {
           )}
 
         </div>
-
-        <button onClick={handleComplatePlayerInfo} disabled={isLoading}>{isLoading ? '情報を取得中...' : 'APIで情報を補完'}</button>
 
         {playerInModal && (
           <RankModal
