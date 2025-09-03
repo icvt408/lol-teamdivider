b9a033b9468ac95c1c87ed842e157bda05888a16
EBlTEN <228321068+icvt408@users.noreply.github.com>
2025-09-03 22:43:33 +0900

    ボタンをコンポーネント化

---
+++ src/components/Button.jsx
@@ -0,0 +1,46 @@
+
+const Button = ({ content, onClick, href, color = "bg-primary", textColor = "text-white", isActive = true, disabled = false }) => {
+    const isLink = !!href;
+
+    const paddingClass = typeof content === 'string' ? 'py-3 px-6' : 'p-2';
+
+    const activeClass = isActive ? "opacity-100" : "opacity-40";
+
+    const baseClasses = `
+    ${paddingClass} rounded-full font-medium transition duration-200
+    hover:opacity-80 active:opacity-60 ${activeClass}
+    `;
+
+    const buttonContent = typeof content === "string" ? (
+        <span className="flex items-center space-x-2">
+            {content}
+        </span>
+    ) : (
+        content
+    );
+
+    if (isLink) {
+        return (
+            <a
+                href={href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`${baseClasses} ${color} ${textColor} no-undeline flex item-center justify-ceonter`}
+            >
+                {buttonContent}
+            </a>
+        )
+    }
+
+    return (
+        <button
+            onClick={onClick}
+            className={`${baseClasses} ${color} ${textColor}`}
+            disabled={disabled}
+        >
+            {buttonContent}
+        </button>
+    );
+};
+
+export default Button
